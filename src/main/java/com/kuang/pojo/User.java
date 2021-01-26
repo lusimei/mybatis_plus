c3b26d5fd474d48c03e651422577231b0e490bf0
chad <chad@mieasy.com>
2021-01-26 18:15:19 +0800

    增删改查

--- src/main/java/com/kuang/pojo/User.java
+++ src/main/java/com/kuang/pojo/User.java
@@ -1,9 +1,6 @@
 package com.kuang.pojo;
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -29,4 +26,8 @@ public class User {
 
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
+
+    @Version
+    private Integer version;
+
 }
--- src/test/java/com/kuang/mybatis_plus/MybatisPlusApplicationTests.java
+++ src/test/java/com/kuang/mybatis_plus/MybatisPlusApplicationTests.java
@@ -1,12 +1,17 @@
 package com.kuang.mybatis_plus;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.kuang.mapper.UserMapper;
 import com.kuang.pojo.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 class MybatisPlusApplicationTests {
@@ -26,8 +31,7 @@ class MybatisPlusApplicationTests {
         user.setAge(12);
         user.setEmail("AAA@163.com");
         user.setName("BBB");
-        int insert = userMapper.insert(user);
-        System.out.println(insert);
+        userMapper.insert(user);
         System.out.println(user);
     }
 
@@ -37,9 +41,54 @@ class MybatisPlusApplicationTests {
         user.setId(1353586686646546443l);
         user.setEmail("CCC@163.com");
         user.setName("TIDY");
-        int insert = userMapper.updateById(user);
-        System.out.println(insert);
+        userMapper.updateById(user);
         System.out.println(user);
     }
 
+    @Test
+    void testOptimisticLocker(){
+        User user = userMapper.selectById(1l);
+        user.setEmail("777@163.com");
+        user.setName("name");
+
+        //模拟另外一个线执行了插队操作
+        User user1 = userMapper.selectById(1l);
+        user1.setEmail("888@163.com");
+        user1.setName("name");
+        user1.setVersion(10);
+
+        userMapper.updateById(user);
+
+        //自旋锁来多次尝试提交
+        userMapper.updateById(user1);//如果没有乐观锁就会覆盖插队线程的值
+
+    }
+
+    @Test
+    public void testSelectById(){
+        User user = userMapper.selectById(1l);
+        System.out.println(user);
+    }
+
+    @Test
+    public void testBatchSelect(){
+        List<User> users = userMapper.selectBatchIds(Arrays.asList(1, 2, 3));
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelectByMap(){
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("name","Jack");
+        hashMap.put("age",20);
+        List<User> users = userMapper.selectByMap(hashMap);
+        users.forEach(System.out::println);
+    }
+
+    @Test
+    public void testPage(){
+        IPage<User> userIPage = userMapper.selectPage(new Page(2,5),null);
+        userIPage.getRecords().forEach(System.out::println);
+        System.out.println(userIPage.getTotal());
+    }
 }
