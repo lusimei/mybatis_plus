e635e3fb69d63decf68f435bc9888d5bced5619c
chad <chad@mieasy.com>
2021-01-27 18:20:56 +0800

    使用方法学习完成

---
+++ src/test/java/com/kuang/mybatis_plus/CodeGenerator.java
@@ -0,0 +1,8 @@
+package com.kuang.mybatis_plus;
+
+public class CodeGenerator {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.dir"));
+    }
+}
